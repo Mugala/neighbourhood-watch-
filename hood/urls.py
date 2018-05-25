cfc35ee1b8f5219814ed10cc5ee91ed607bca663
mugala <mugalalevi21@gmail.com>
2018-05-25 15:22:11 +0300

    Templates This commit will register the app in the settings as well as create the first template for the home page

--- hood/urls.py
+++ hood/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome'),
+    url('^$', views.home, name = 'home'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def welcome(request):
-    return HttpResponse("Welcome to Neighbourhood Watch")
+def home(request):
+    return render(request, "home.html")
