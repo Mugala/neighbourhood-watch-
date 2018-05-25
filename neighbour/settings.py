1aebb33a85ecdd299ad65154068fa43ebe0ce154
mugala <mugalalevi21@gmail.com>
2018-05-25 17:26:25 +0300

    Database This commit will install the psycopg2 dependancy that will help link the project to the database hoodwatch

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'neighbour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoodwatch',
+        'USER': 'levi',
+        'PASSWORD': 'eyeshadow',
     }
 }
 
