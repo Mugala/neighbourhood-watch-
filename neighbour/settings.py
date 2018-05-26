9f87d0e530b8c0dc0818b776c3294e4c2e4215bd
mugala <mugalalevi21@gmail.com>
2018-05-26 16:52:22 +0300

    Update settings.py and wsgi.py This commit will update the settings.py and the wsgi.py with the necessary code for deployment

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,14 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+ ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE ='whitenoise.django.GzipManifestStaticFilesStorage'
